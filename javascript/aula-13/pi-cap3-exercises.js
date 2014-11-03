6b19b2ac0b94a161bddda87ecb4d5405db73f41e
Carlos Guedes <cguedes@gmail.com>
2014-11-03 23:04:12 +0000

    Código da aula 13

--- javascript/aula-13/pi-cap3-exercises.js
+++ javascript/aula-13/pi-cap3-exercises.js
@@ -4,33 +4,92 @@ module("Cap 3 - Exercícios", function cap4(assert)
   assert(true, "123 Go....");
 
   // function - average
+  function average() {
+    var res = 0;
+    for(var i = 0; i < arguments.length; ++i) {
+      res += arguments[i];
+    }
+    return res / arguments.length;
+  }
   assert( average(1,2,3) == 2, "the average of 1,2,3 is 2" );
   assert( average(1,2,3,4,5,6) == 3.5, "the average of 1,2,3,4,5,6 is 3.5" );
 
-
   // function - isPalindrome
+  function isPalindromeIterative(text) 
+  {
+    for(var i = 0, j = text.length - 1; i < text.length / 2; ++i, --j) {
+      //if(text[i] != text[j]) return false;
+      if(text.charAt(i) != text.charAt(j)) return false;
+    }
+    return true;
+  }
+  function isPalindrome(text) {
+    if(text.length <= 1) return true;
+    if(text.charAt(0) != text.charAt(text.length-1)) return false;
+    return isPalindrome(text.substring(1, text.length-1));
+  }
+
   assert( true == isPalindrome("anana"), "anana is a palindrome");
   assert( false == isPalindrome("ababba"), "ababba is not a palindrome");
 
   // method - isGreaterThanPropertyX
   var obj1 = { x: 4 };
+  obj1.xIsGreaterThan = function(val) {
+    return this.x > val;
+  }
+
   assert(obj1.xIsGreaterThan(+2) == true,  "o.x = 4, that is greater that 2");
   assert(obj1.xIsGreaterThan(+6) == false, "o.x = 4, that is not greater that 6");
 
-  var obj2 = { x: 1 };
-  obj2.xIsGreaterThan = obj2.xIsGreaterThan;
+  var obj2 = { x: 1, xIsGreaterThan: obj1.xIsGreaterThan };
   assert(obj2.xIsGreaterThan(+2) == false, "o.x = 1, that is not greater that 2");
-  assert(obj2.xIsGreaterThan(-2) == false, "o.x = 1, that is greater that -2");
+  assert(obj2.xIsGreaterThan(-2) == true, "o.x = 1, that is greater that -2");
 
   // constructor
-  var bob = new Animal("Bob");
-  assert("Bob" == bob.getName(), "bob is called Bob" );
+  function Animal(firstName, lastName) 
+  {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.getName = function() 
+      { 
+        return this.firstName + " " + this.lastName;
+      };
+  }
+  var bob = new Animal("Bob", "Cat");
+  assert("Bob Cat" == bob.getName(), "bob is called Bob Cat" );
+
+  var thor = new Animal("Thor", "The Silly Dog!");
+  thor.lastName = "The Warrior!";
+  assert("Thor The Silly Dog!" != thor.getName(), "thor is used to be called Thor The Silly Dog!" );
+
+  var gn = thor.getName;
+  assert(true, gn());
+
+  var gn = thor.getName;
+  assert(true, gn.call(thor));
+  assert(true, gn.apply(bob));
 
   // apply / call
   var grades = [10, 20, 10, 20];
-  assert( 15 == average.apply(/*...*/), "average of 10,20,10,20 is 15" );
+  assert( 15 == average.apply(null, grades), "average of 10,20,10,20 is 15" );
   var f = obj1.xIsGreaterThan;
-  assert( true == f...., "call f with obj1 and value 0 as argument and get true" );
+  assert( true == f.call(obj1, 0) /* <=> obj1.f(0) */, "call f with obj1 and value 0 as argument and get true" );
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- javascript/aula-13/pi-cap4.js
+++ javascript/aula-13/pi-cap4.js
@@ -3,6 +3,10 @@ module("Cap 4", function cap4(assert)
   
   assert(true, "Ok");
 
+  setTimeout(function()  { assert(true, "Executed 2ms after Ok") }, 2);
+  //setInterval(function() { assert(true, "Executed 500ms after Ok") }, 500);
+  assert(true, "Another Ok");
+
   // 4.1 anonymous functions
   //       window.onload
   //       method definition
@@ -17,6 +21,37 @@ module("Cap 4", function cap4(assert)
   //       storing functions
   //       self-memoizing functions
   //       faking array methods
+  var cache = {};
+  function memoize(fn) 
+  {
+    return function(v) 
+    {
+      if(cache[v] != undefined) return cache[v];
+      return cache[v] = fn(v);
+    }
+  }
+
+  function isPrime(value) {
+    console.log("isPrime(" + value + ")" );
+    var prime = value != 1;
+    for (var i = 2; i < value; i++) {
+      if (value % i == 0) {
+        prime = false;
+        break;
+      } 
+    }
+    return prime;
+  }
+
+  var isPrimeMemoized = memoize(isPrime);
+
+  assert(isPrimeMemoized(5), "5 is prime");
+  assert(isPrimeMemoized(5), "5 is prime");
+
+
+  function numberToString(num) {
+    return "" + num;
+  }
 
   // 4.4 Variable-length arguments lists
   //       with apply
@@ -25,4 +60,12 @@ module("Cap 4", function cap4(assert)
   // 4.5 Checking for functions
 
 });
+
+
+
+
+
+
+
+
 
