0208f7c806b251100984449b70a6f2714372ae87
Carlos Guedes <cguedes@gmail.com>
2014-12-15 22:00:02 +0000

    Usa cookie-parser

--- nodejs/aula24/01-cookies/server.js
+++ nodejs/aula24/01-cookies/server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var favicon = require("serve-favicon");
 var morgan = require("morgan");
 
+var cookieParser = require("cookie-parser");
+
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -11,16 +14,18 @@ app.set('views', './views');
 app.use(morgan("dev"));
 app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", function(req, res) 
 {
-  res.render("home", { headers: req.headers, cookies: req.headers["cookie"] });
+  res.render("home", { headers: req.headers, cookies: req.cookies });
 });
 
 
 app.get("/getcookie", function(req, res) 
 {
     res.setHeader("Set-Cookie", "useid=123EFrT2");
+    console.log(res.cookies);
     res.send("Ok. Cookie sent.");
 });
 
